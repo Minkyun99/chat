390461066d86ab769bd14b6af1b21c0f13f26467
Minkyun99 <alsrud7412@naver.com>
2023-12-03 17:14:49 +0900

    1203

--- src/router/index.js
+++ src/router/index.js
@@ -18,9 +18,9 @@ const routes = [
   },
 ];
 
-// const router = createRouter({
-//   history: createWebHistory(process.env.BASE_URL),
-//   routes,
-// });
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
 
 export default router;
