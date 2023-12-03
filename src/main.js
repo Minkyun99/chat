171d1a5f294c40d3173eea9d6e463972349aeb91
Minkyun99 <alsrud7412@naver.com>
2023-12-03 18:35:57 +0900

    1203

--- src/main.js
+++ src/main.js
@@ -4,10 +4,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import { io } from "socket.io-client";
+// import { io } from "socket.io-client";
 
 const app = createApp(App);
-const socket = io();
-app.config.globalProperties.$socket = socket;
+// const socket = io();
+// app.config.globalProperties.$socket = socket;
 
 app.use(store).use(router).mount("#app");
