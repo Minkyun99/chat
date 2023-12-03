01aa18292a68aeb91794b99d11b0ce596f64d61f
Minkyun99 <alsrud7412@naver.com>
2023-12-03 16:30:24 +0900

    1203

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,12 @@ import HomeView from "../views/HomeView.vue";
 
 const routes = [
   {
-    path: "/",
+    path: "/https://web-chat-32updzt2alpp3tqt2.sel4.cloudtype.app",
     name: "home",
     component: HomeView,
   },
   {
-    path: "/about",
+    path: "https://web-chat-32updzt2alpp3tqt2.sel4.cloudtype.app/about",
     name: "about",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
